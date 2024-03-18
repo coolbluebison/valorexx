24a03b1321eaf1c97781cf63b11cf94a704c8a54
Tri <trihoang09@gmail.com>
2024-03-18 15:40:46 -0600

    updated template

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, render_template
 from flask_restful import Resource
 
 # Local imports
@@ -11,12 +11,21 @@ from config import app, db, api
 # Add your model imports
 
 
-# Views go here!
+
 
 @app.route('/')
 def index():
-    return '<h1>Project Server</h1>'
+    return '<h1>Phase 4 Project Server</h1>'
 
+# Views go here! use either route!
+# @app.errorhandler(404)
+# def not_found(e):
+#     return render_template("index.html")
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
